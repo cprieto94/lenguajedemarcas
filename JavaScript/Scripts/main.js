cc3b393f8b074966f135db5d3886314bd91a4e3c
“cprieto94” <cristianzgz13@gmail.com>
2023-01-13 13:19:29 +0100

    mas cambios de javascript

--- JavaScript/Scripts/main.js
+++ JavaScript/Scripts/main.js
@@ -1,1 +1,12 @@
-const miTitulo = document.querySelector('h1');
+function esPrimo(n) {
+
+    for (i=2; i<n; i++){
+
+        if (n%i === 0){
+
+           return false; 
+
+        }
+    }
+    return true;
+}
