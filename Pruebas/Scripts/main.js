f7fe0b7b698985c6b7cf1f29e3ff0cca5ef27e60
“cprieto94” <cristianzgz13@gmail.com>
2023-01-20 13:53:44 +0100

    pruebas con mas scripts

--- Pruebas/Scripts/main.js
+++ Pruebas/Scripts/main.js
@@ -1,11 +1,9 @@
-window.alert("Hola a todos y todas")
-
 function manejadorLoadBody(){
     console.log("Se ha cargado el body");
     peticionAsincrona();
 }
 
-function peticionAsincrona(){
+function peticionAsincrona(funcionParaCallback){
     const url="https://www.zaragoza.es/sede/servicio/farmacia?rf=html&srsname=wgs84&tipo=guardia&start=0&rows=50&distance=500";
     const http = new XMLHttpRequest();
     http.open("GET", url);
@@ -13,9 +11,39 @@ function peticionAsincrona(){
     http.setRequestHeader("accept", "application/json");
     http.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200){
-            console.log (this.responseText);
-            let resultado = JSON.parse(this.responseText)
+            
+            let resultado = JSON.parse(this.responseText);
+            console.log(resultado);
+            pintarFarmacias(resultado);
         }
     }
     http.send();
+}
+
+function pintarFarmacias(objFarmacias){
+
+    let contenedor = document.getElementById("listaFarmacias");
+    let arrayFarmacias = objFarmacias.result;
+    let contenido ='<ul>';
+
+    arrayFarmacias.forEach(f => {
+        contenido += '<li>' + f.title + '</li>';
+    });
+    contenido += '</ul>';
+
+    contenedor.innerHTML += contenido;
+}
+
+function pintarFarmaciasMejor(objFarmacias){
+
+    let contenedor = document.getElementById("listaFarmacias");
+    let arrayFarmacias = objFarmacias.result;
+    let contenido ='<ol>';
+
+    arrayFarmacias.forEach(f => {
+        contenido += '<li><h4>' + f.title + '</h4></li>';
+    });
+    contenido += '</ol>';
+
+    contenedor.innerHTML += contenido;
 }
